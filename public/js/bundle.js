eeb6ab54b125609a1ff815a5882522d99d7f5d86
tafseer-khan <tafseer.k.khan@gmail.com>
2021-02-25 10:51:48 -0600

    Remade bundle

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5412,20 +5412,17 @@ finishMenu.addEventListener('click', (e) => {
         res.json().then(data => ({
             id: data.id
         }
+        
         ))
     }).then(
-        QRCode.toString("localhost:8080/menu"+`${id}`, { type: 'terminal' }, (err, url) => {
+        QRCode.toString("https://menumaqr.herokuapp.com/public/menus"+`${id}`+".html", { type: 'terminal' }, (err, url) => {
             console.log(url)
             console.log(id)
             const newQR ={
                 qrLink: url
             }  
             console.log(JSON.stringify(newQR))
-            // connection.query('UPDATE FROM menu_maqrdb SET ? WHERE ?',
-            // [
-            //     {qr: "nice"},
-            //     {menuName: `${mostRecent}`}
-            // ])
+
 
             fetch(`/api/maybe/${mostRecent}/addQR`, {
                     method: 'PUT',
